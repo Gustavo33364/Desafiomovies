69908ca2a92af85f42062b263fe56898bc49c9ee
Gustavo33364 <gustavoayala.ceo@gmail.com>
2022-06-22 19:53:36 -0300

    Cambios

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import styles from "./App.module.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Navigate,
+} from "react-router-dom";
+import { MovieDetails } from "./pages/MovieDetails";
+import { LandingPage } from "./pages/LandingPage";
+
+export function App() {
+  return (
+    <Router>
+      <header>
+        <Link to="/">
+          <h1 className={styles.title}>Tu peli hoy</h1>
+          <h2 className={styles.title2}>La opcion que buscas a un clic</h2>
+        </Link>
+      </header>
+      <main>
+        <Routes>
+          <Route path="/movies/:movieId" element={<MovieDetails />} />
+          <Route path="/" element={<LandingPage />} />
+          <Route path="*" element={<Navigate replace to="/" />} />
+        </Routes>
+      </main>
+    </Router>
+  );
+}
